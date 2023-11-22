51d4f11e8124e9f7fb2263509405ad28f9faf226
omargtx2 <omargtx2@yahoo.es>
2023-11-22 17:53:18 -0500

    new file:   shooter-pygame-master/01_jugador.py 	new file:   shooter-pygame-master/02_enemigos.py 	new file:   shooter-pygame-master/03_colisiones_laser.py 	new file:   shooter-pygame-master/04_marcador.py 	new file:   shooter-pygame-master/05_meteoros_mas.py 	new file:   shooter-pygame-master/06_sonidos_musica.py 	new file:   shooter-pygame-master/07_escudos.py 	new file:   shooter-pygame-master/08_explosions.py 	new file:   shooter-pygame-master/09_gameover.py 	new file:   shooter-pygame-master/README.md 	new file:   shooter-pygame-master/assets/background.png 	new file:   shooter-pygame-master/assets/explosion.wav 	new file:   shooter-pygame-master/assets/laser1.png 	new file:   shooter-pygame-master/assets/laser5.ogg 	new file:   shooter-pygame-master/assets/meteorGrey_big1.png 	new file:   shooter-pygame-master/assets/meteorGrey_big2.png 	new file:   shooter-pygame-master/assets/meteorGrey_big3.png 	new file:   shooter-pygame-master/assets/meteorGrey_big4.png 	new file:   shooter-pygame-master/assets/meteorGrey_med1.png 	new file:   shooter-pygame-master/assets/meteorGrey_med2.png 	new file:   shooter-pygame-master/assets/meteorGrey_small1.png 	new file:   shooter-pygame-master/assets/meteorGrey_small2.png 	new file:   shooter-pygame-master/assets/meteorGrey_tiny1.png 	new file:   shooter-pygame-master/assets/meteorGrey_tiny2.png 	new file:   shooter-pygame-master/assets/music.ogg 	new file:   shooter-pygame-master/assets/player.png 	new file:   shooter-pygame-master/assets/regularExplosion00.png 	new file:   shooter-pygame-master/assets/regularExplosion01.png 	new file:   shooter-pygame-master/assets/regularExplosion02.png 	new file:   shooter-pygame-master/assets/regularExplosion03.png 	new file:   shooter-pygame-master/assets/regularExplosion04.png 	new file:   shooter-pygame-master/assets/regularExplosion05.png 	new file:   shooter-pygame-master/assets/regularExplosion06.png 	new file:   shooter-pygame-master/assets/regularExplosion07.png 	new file:   shooter-pygame-master/assets/regularExplosion08.png 	new file:   shooter-pygame-master/main.py 	new file:   shooter-pygame-master/shooter.py

---
+++ shooter-pygame-master/main.py
@@ -0,0 +1,49 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class Student:
+   def __init__(self, name, rollno):
+       self.name=name
+       self.rollno=rollno
+   def __str__(self):
+       return 'This is Student object with name {} and roll no {}'.format(self.name, self.rollno)
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def main():
+    preguntas = { "¿Cual es el animal mas grande del mundo": "la ballena azul",
+              "¿Cual es el planeta mas cercano al sol?": "Mercurio",
+              "¿Que animal tiene la piel da rayas negras y blancas":"La cebra"}
+
+    puntos=0
+
+
+
+    for pregunta , respuesta in preguntas.items():
+        print(pregunta)
+        respuesta_usuario = input("Tu respuesta:")
+
+        if respuesta_usuario.lower()==respuesta.lower():
+            print("¡Correcto")
+            puntos+=1
+        else:
+            print("Incorrecto , la repsuesta correcta es : " + respuesta)
+
+    print(f"Finalizo el juego , obtuviste {puntos} puntos ")
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #print_hi('PyCharm')
+    main()
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
